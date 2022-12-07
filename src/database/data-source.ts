6d1e6976901c3dea13549932a28602f20084aab6
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-12-06 22:29:02 -0300

    feat: add docker-compose

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -8,9 +8,9 @@ export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: port,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  username: 'bruno',
+  password: 'root',
+  database: 'agraneldb',
   entities: [`${__dirname}/**/entities/*.{ts,js}`],
   migrations: [`${__dirname}/**/database/migrations/*.{ts,js}`],
 });
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+import 'dotenv/config';
 import express from 'express';
 import { AppDataSource } from './database/data-source';
 
