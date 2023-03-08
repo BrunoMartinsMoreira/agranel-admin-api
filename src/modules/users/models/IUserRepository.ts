41e25897a4bcf4b07ecca6f6e379857b2970371f
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2023-03-08 12:38:39 -0300

    refactor: refactor to nestjs

--- src/modules/users/models/IUserRepository.ts
+++
@@ -1,6 +0,0 @@
-import { ICreateUserDto } from '../dto/ICreateUser.dto';
-import { User } from '../infra/database/entities/User.entity';
-
-export interface IUserRepository {
-  create: (data: ICreateUserDto) => Promise<User>;
-}
--- src/shared/containers/index.ts
+++
@@ -1,8 +0,0 @@
-import { UsersRepository } from 'modules/users/infra/database/repositories/User.repository';
-import { IUserRepository } from 'modules/users/models/IUserRepository';
-import { container } from 'tsyringe';
-
-container.registerSingleton<IUserRepository>(
-  'UsersRepository',
-  UsersRepository,
-);
