c8f0f4e5668cd0ea95443f01eac0d9dbc0b6c6e4
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2023-04-06 20:38:05 -0300

    feat: corrige resposta de item criado e erro desconhecido

--- src/common/types/DefaultMessages.ts
+++ src/common/types/DefaultMessages.ts
@@ -1,7 +1,7 @@
 export enum DefaultMessages {
   QUERY_SUCCESS = 'Consulta realizada com sucesso.',
-  CREATED = 'Criado com sucesso.',
-  UNKNOWN_ERROR = 'Houve um erro desconhecido, por favor, tente novamente. Se o erro persistir, entre em contato com o administrador do sistema.',
+  CREATED = 'Cadastrado com sucesso.',
+  UNKNOWN_ERROR = 'Deu ruim e nem eu sei o que houve, só nos resta rezar!',
   NAME_REQUIRED = 'Campo nome é obrigatório',
   NAME_STRING = 'Campo nome deve ser uma string',
   NAME_LENGTH = 'Campo nome deve ter no máximo 254 caraceters',
